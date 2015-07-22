edf33b7449c4ae28f5c3a73b4e2f5ff19853e09b
wojjas <wojtek@proxoft.se>
2015-07-22 16:38:59 +0200

    Ops, small error intorduced in last commit

--- client/auth/oauth.js
+++ client/auth/oauth.js
@@ -17,16 +17,13 @@
         ////////////////
 
         function login(username, password) {
-            //If not in debug-mode we are probably running on localhost or the developer knows what he's doing, no need for this check.
-            if(process.env.IN_PRODUCTION_MODE){
-                if($window.location.protocol !== 'https:'){
-                    $window.alert('Attempted to send credentials not using https. \n' +
-                    'This is unsafe and not allowed.');
+            if($window.location.protocol !== 'https:'){
+                $window.alert('Attempted to send credentials not using https. \n' +
+                'This is unsafe and not allowed.');
 
-                    return $q.reject({statusText:'Login attempt canceled at client. Tried to use unsafe protocol for sending credentials.'});
-                }
+                return $q.reject({statusText:'Login attempt canceled at client. Tried to use unsafe protocol for sending credentials.'});
             }
-
+            
             var config = {
                 headers: {
                     "Content-Type": "application/x-www-form-urlencoded"
